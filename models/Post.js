aee8859db38041563e8382eaaabcacae7eadd454
mlopez <lopezmatthew87@gmail.com>
2021-07-30 02:31:23 -0400

    updating css

--- models/Post.js
+++ models/Post.js
@@ -49,9 +49,7 @@ Post.init(
     post_url: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isURL: true
-      }
+
     },
     user_id: {
       type: DataTypes.INTEGER,
